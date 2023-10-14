ff6c501d01b3855b9a87f3db0f31634804690206
Ariet <147935383+culun06me@users.noreply.github.com>
2023-10-15 00:32:51 +0700

    Add files via upload

---
+++ Install-Putty.cmd
@@ -0,0 +1,12 @@
+@echo off
+rem script name.
+set "script_name=Install-Putty.ps1"
+
+rem script directory.
+set "script_directory=%~dp0"
+
+rem execute PS1 script.
+powershell -executionpolicy bypass -file "%script_directory%%script_name%"
+
+rem pause, press any key to exit.
+pause
